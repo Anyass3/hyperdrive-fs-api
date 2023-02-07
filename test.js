e73dd0447901bf131d8964289ca5ea261deceba6
Abdoulie Nyass <nyassabu@gmail.com>
2023-02-07 03:27:51 +0000

    add folders/directories tests

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-console.log('testing...')
+import test from 'brittle'
 import Corestore from 'corestore';
 import RAM from 'random-access-memory'
 import Hyperdrive from './lib/index.js';
+
+const getDrive = () => {
+
+    const corestore = new Corestore((() => {
+        return new RAM();
+    }))
+
+    return new Hyperdrive(corestore);
+}
 
 async function main() {
 
@@ -25,5 +34,18 @@ async function main() {
     console.log(await drive.get('/a/b/'));
 }
 
+console.log('testing...');
 
-main()
+test('create folders/directories', async t => {
+    const drive = getDrive();
+    await drive.mkdir('/dir1/dir2/dir3');
+    const list1 = await drive.list('/', { recursive: true });
+    const list2 = await drive.list('/', { recursive: false });
+    const list3 = await drive.list('/', { recursive: false, stat: true });
+    t.is(list1.length, 3)
+    t.is(list2.length, 1)
+    t.ok(await drive.exists('/dir1/dir2'))
+    t.absent(await drive.exists('/dir1/dir'))
+    t.absent(list1[0].stat)
+    t.ok(list3[0].stat)
+})

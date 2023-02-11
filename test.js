640366ee220c4863db8652adf611956bb9e4230f
Abdoulie Nyass <nyassabu@gmail.com>
2023-02-11 14:12:56 +0000

    add tests for Coping and moving files and stream folders/dirs

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import test from 'brittle'
 import Corestore from 'corestore';
 import RAM from 'random-access-memory'
+import { pipelinePromise } from 'streamx';
 import Hyperdrive from './lib/index.js';
 
 const getDrive = () => {
@@ -19,16 +20,18 @@ async function main() {
     }))
 
     const drive = new Hyperdrive(corestore);
-    await drive.put('/a/b', Buffer.from('bvbv'))
+    await drive.put('/a/b.txt', Buffer.from('bvbv'))
     await drive.put('/a/c', Buffer.from(''))
     await drive.put('/a/d/p', Buffer.from(''))
     await drive.put('/a/e/d', Buffer.from(''))
     await drive.put('/a/b/r', Buffer.from(''))
     await drive.put('/a/c/p/', Buffer.from(''))
     await drive.put('/a/b/d/', Buffer.from(''))
-    console.log('list', await drive.list('/a', { recursive: false }))
+
+    await drive.move('/a/b.txt', '/moved.txt')
+    console.log('list', await drive.list('/', { recursive: true }))
     // console.log('dir', await drive.list('/a/b'));
-    console.log('dir', await drive.list('/a', { stat: true }));
+    // console.log('dir', await drive.list('/a', { stat: true }));
     // console.log(await drive.entry('/a/b'));
     // console.log(await drive.get('/a/b/'));
 }
@@ -51,6 +54,7 @@ test('create files/dirs', async t => {
     t.comment('comparing buffer to buffer')
     t.alike(Buffer.from('hi there'), await drive.get('/dir1/dir2/file.txt'))
 })
+
 test('files/dirs stats', async t => {
     const drive = getDrive();
     await drive.write('/dir1/dir2/file.txt', 'hi there', 'utf-8');
@@ -59,9 +63,37 @@ test('files/dirs stats', async t => {
     const list1 = await drive.list('/', { recursive: true });
     const list2 = await drive.readdir('/dir1', { stat: true });
 
-    // console.log(list2, list1)
     t.absent(list1[0].stat)
     t.ok(list2[0].stat)
     t.ok((await drive.stat('/dir1/dir2/file.txt')).birthtimeMs < (await drive.stat('/dir1/file.txt')).birthtimeMs)
     t.ok((await drive.stat('/dir1/dir2/')).mtimeMs < (await drive.stat('/dir1/')).mtimeMs)
+})
+
+test('Coping and moving files', async t => {
+    const drive = getDrive();
+    await drive.write('/dir1/dir2/file.txt', 'hi there', 'utf-8');
+    await drive.copy('/dir1/dir2/file.txt', '/copyed.txt');
+
+    t.is(await drive.get('/dir1/dir2/file.txt').toString(), await drive.get('/copyed.txt').toString())
+
+    await drive.move('/dir1/dir2/file.txt', '/moved.txt');
+
+    t.not(await drive.exists('/dir1/dir2/file.txt'))
+
+    t.ok(await drive.exists('/moved.txt'))
+})
+
+test('stream folders/dirs', async t => {
+    const drive = getDrive();
+    await drive.write('/dir1/abc.txt', 'abcdefg', 'utf-8');
+    await drive.write('/dir1/dir2/hi.txt', 'hi me', 'utf-8');
+    await drive.write('/dir1/doc/greeting.txt', 'hello there! How are you', 'utf-8');
+
+    await pipelinePromise(drive.createFolderReadStream('/dir1/'), drive.createFolderWriteStream('/streamed/'))
+
+    const list1 = await drive.list('/dir1', { recursive: true, });
+    const list2 = await drive.list('/streamed', { recursive: true });
+    const mapper = ({ name, path }) => ({ name, path: path.replace(/^\/((dir1)|(streamed))/, '') });
+
+    t.alike(list1.map(mapper), list2.map(mapper));
 })
